2e2b65c5c4b52bf6fa2cb599cae93901918d93c0
Stefano Vannucchi <stefano.vannucchi1@gmail.com>
2020-06-30 17:46:21 +0200

    fixed first failing test for CardBack component

---
+++ src/components/CardBack.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flip-card-back">
+        <div class="ui card">
+            <div class="content"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CardBack"
+    }
+</script>
+
+<style scoped>
+
+</style>
