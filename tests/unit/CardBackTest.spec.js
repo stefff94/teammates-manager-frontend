8a34b8fb281635e89a7dd079a457ab43a9dc3d7f
Stefano Vannucchi <stefano.vannucchi1@gmail.com>
2020-07-01 10:18:47 +0200

    another failing test for CardBack component

--- tests/unit/CardBackTest.spec.js
+++ tests/unit/CardBackTest.spec.js
@@ -1,5 +1,5 @@
 import { shallowMount } from "@vue/test-utils";
-import CardBack from "@/components/CardBack";
+import CardBack from "../../src/components/CardBack";
 
 let wrapper = null;
 
@@ -14,7 +14,15 @@ afterEach(() => {
 describe("CardBack.vue", () => {
 
    it("renders the card back", () => {
-       expect(wrapper.find(".flip-card-back .ui.card .content"));
+       expect(wrapper.find(".flip-card-back .ui.card .content")
+           .exists())
+           .toBeTruthy();
+   });
+
+   it("renders the delete icon button", () => {
+       expect(wrapper.find(".content .delete.right.floated.large.x.icon")
+           .exists())
+           .toBeTruthy();
    });
 
 });
