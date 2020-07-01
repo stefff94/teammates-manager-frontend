3b8f73f3a066b293cdb67684d4c9e4b40481574a
Stefano Vannucchi <stefano.vannucchi1@gmail.com>
2020-07-01 12:36:42 +0200

    other failing tests for Card component with stubbed child components

--- tests/unit/CardTest.spec.js
+++ tests/unit/CardTest.spec.js
@@ -1,10 +1,22 @@
 import { shallowMount } from "@vue/test-utils";
 import Card from "../../src/components/Card";
+import CardBack from "../../src/components/CardBack";
+import CardFront from "../../src/components/CardFront";
 
 let wrapper = null;
 
+// shallowMount method auto stubs child components of mounted component
+// in our case it automatically stubs CardBack and CardFront component
 beforeEach(() => {
-    wrapper = shallowMount(Card);
+    wrapper = shallowMount(Card, {
+        propsData: {
+            person: {
+                personalData: {},
+                skills: [],
+                id: 1
+            }
+        }
+    });
 });
 
 afterEach(() => {
@@ -18,6 +30,25 @@ describe("Card.vue", () => {
             .find(".flip-card .flip-card-inner")
             .exists())
             .toBeTruthy();
+    });
+
+    it("renders the card front", () => {
+        expect(wrapper.find(CardFront).exists())
+            .toBeTruthy();
+
+        expect(wrapper.find(CardFront).props("data"))
+            .toBe(wrapper.vm.person.personalData);
+    });
+
+    it("renders the card back", () => {
+        expect(wrapper.find(CardBack).exists())
+            .toBeTruthy();
+
+        expect(wrapper.find(CardBack).props("skills"))
+            .toBe(wrapper.vm.person.skills);
+
+        expect(wrapper.find(CardBack).props("id"))
+            .toBe(wrapper.vm.person.id);
     });
 
 });
