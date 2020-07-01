c6218d3a6fcb3a30d04671000ea169a92b06a2ac
Stefano Vannucchi <stefano.vannucchi1@gmail.com>
2020-07-01 11:16:20 +0200

    fixed first test for CardFront component

---
+++ src/components/CardFront.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flip-card-front">
+        <div class="ui card">
+            <div class="content"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CardFront"
+    }
+</script>
+
+<style scoped>
+
+</style>
