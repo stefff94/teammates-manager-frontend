af8b26556509235dece546125ff2d1912dc80bb4
Stefano Vannucchi <stefano.vannucchi1@gmail.com>
2020-07-01 12:27:23 +0200

    first failing test for Card component

---
+++ tests/unit/CardTest.spec.js
@@ -0,0 +1,23 @@
+import { shallowMount } from "@vue/test-utils";
+import Card from "../../src/components/Card";
+
+let wrapper = null;
+
+beforeEach(() => {
+    wrapper = shallowMount(Card);
+});
+
+afterEach(() => {
+    wrapper.destroy();
+});
+
+describe("Card.vue", () => {
+
+    it("renders the flip card", () => {
+        expect(wrapper
+            .find("flip-card .flip-card-inner")
+            .exists())
+            .toBeTruthy();
+    });
+
+});
