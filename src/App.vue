d6a00ad2f0fff0344c14079687cbfddb9bb651b1
Stefano Vannucchi <stefano.vannucchi1@gmail.com>
2020-07-01 15:33:46 +0200

    fixed tests for App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,35 @@
 
     <div class="ui divider"></div>
 
+    <h2 class="ui center aligned icon header">
+      <i class="circular users icon"></i>
+      Teammates
+    </h2>
+
+    <div class="ui three column stackable grid mt35">
+      <card class="column"
+            v-for="teammate in teammates"
+            :person="teammate"
+            v-bind:key="teammate.id"
+            style="margin-bottom: 20px!important;"></card>
+    </div>
+
   </div>
 </template>
 
 <script>
+import Card from "./components/Card";
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Card
+  },
+  data() {
+    return {
+      teammates: []
+    }
+  }
 }
 </script>
 
