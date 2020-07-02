360b5bf228a2df310b33b11a6901b50a300b6320
Stefano Vannucchi <stefano.vannucchi1@gmail.com>
2020-07-02 14:33:48 +0200

    refactoring tests

--- tests/unit/AppTest.spec.js
+++ tests/unit/AppTest.spec.js
@@ -3,48 +3,53 @@ import App from "../../src/App";
 import Card from "../../src/components/Card";
 
 let wrapper = null;
+let teammates = [];
 
 beforeEach(() => {
+
+    teammates = [
+        {
+            id: 1,
+            personalData: {
+                photoUrl: "/images/avatar/large/steve.jpg",
+                name: "Stefano Vannucchi",
+                role: "Student",
+                email: "stefano.vannucchi@stud.unifi.it",
+                city: "Prato"
+            },
+            skills: [
+                { id: "1", name: "Java" },
+                { id: "2", name: "Spring Boot" },
+                { id: "3", name: "Javascript" },
+                { id: "4", name: "Vue js" }
+            ],
+        },
+        {
+            id: 2,
+            personalData: {
+                photoUrl: "/images/avatar/large/matthew.jpg",
+                name: "Paolo Innocenti",
+                role: "Student",
+                email: "paolo.innocenti@stud.unifi.it",
+                city: "Pistoia"
+            },
+            skills: [
+                { id: "1", name: "Java" },
+                { id: "2", name: "Spring Boot" },
+                { id: "3", name: "Javascript" },
+                { id: "4", name: "Vue js" }
+            ],
+        }
+    ]
+
     wrapper = shallowMount(App, {
         data: () => {
             return {
-                teammates: [
-                    {
-                        id: 1,
-                        personalData: {
-                            photoUrl: "/images/avatar/large/steve.jpg",
-                            name: "Stefano Vannucchi",
-                            role: "Student",
-                            email: "stefano.vannucchi@stud.unifi.it",
-                            city: "Prato"
-                        },
-                        skills: [
-                            { id: "1", name: "Java" },
-                            { id: "2", name: "Spring Boot" },
-                            { id: "3", name: "Javascript" },
-                            { id: "4", name: "Vue js" }
-                        ],
-                    },
-                    {
-                        id: 2,
-                        personalData: {
-                            photoUrl: "/images/avatar/large/matthew.jpg",
-                            name: "Paolo Innocenti",
-                            role: "Student",
-                            email: "paolo.innocenti@stud.unifi.it",
-                            city: "Pistoia"
-                        },
-                        skills: [
-                            { id: "1", name: "Java" },
-                            { id: "2", name: "Spring Boot" },
-                            { id: "3", name: "Javascript" },
-                            { id: "4", name: "Vue js" }
-                        ],
-                    }
-                ]
+                teammates: teammates
             }
         }
     });
+
 });
 
 describe("App.vue", () => {
