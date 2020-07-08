92253324ed4298dfcf2ae29f8e6ed3f9cfa9c9c6
Stefano Vannucchi <stefano.vannucchi1@gmail.com>
2020-07-08 17:45:40 +0200

    other integration tests for App and Card components

--- tests/it/AppIT.spec.js
+++ tests/it/AppIT.spec.js
@@ -1,9 +1,13 @@
 import { mount } from "@vue/test-utils";
 import App from "../../src/App";
 import Card from "../../src/components/Card";
+import ApiService from "../../src/services/api.service";
 
 let wrapper = null;
 let teammates = null;
+let newTeammate = null;
+
+jest.mock("../../src/services/api.service");
 
 beforeEach(() => {
     let skills = [
@@ -39,6 +43,30 @@ beforeEach(() => {
             skills: skills,
         }
     ];
+
+    newTeammate = {
+        name: {
+            value: "Stefano Vannucchi"
+        },
+        gender: {
+            value: "M"
+        },
+        email: {
+            value: "stefano.vannucchi@stud.unifi.it"
+        },
+        city: {
+            value: "Prato"
+        },
+        role: {
+            value: "Student"
+        },
+        skills: [
+            { id: "1", name: "Java" },
+            { id: "2", name: "Spring Boot" },
+            { id: "3", name: "Javascript" },
+            { id: "4", name: "Vue js" }
+        ]
+    }
 });
 
 describe("App.vue", () => {
@@ -61,6 +89,65 @@ describe("App.vue", () => {
             expect(teammates)
                 .toContain(card.vm.person);
         });
+    });
+
+});
+
+describe("The teammate is deleted", () => {
+
+    beforeEach(() => {
+        const resp = { data: teammates };
+
+        ApiService.getAllTeammates.mockResolvedValue(resp);
+        ApiService.deleteTeammate.mockResolvedValue(null);
+
+        wrapper = mount(App, {
+            data: () => {
+                return {
+                    teammates: []
+                }
+            }
+        });
+    });
+
+    it("delete the teammate", async () => {
+        const teammateToDelete = teammates[0].id;
+
+        await wrapper.findAllComponents(Card).wrappers[0]
+            .vm.deleteTeammate(teammateToDelete);
+
+        expect(wrapper.vm.teammates
+            .find(t => t.id === teammateToDelete))
+            .toBeUndefined();
+    });
+
+});
+
+describe("The teammate is being updated after performing the edit operation", () => {
+
+    beforeEach(() => {
+        const resp = { data: teammates };
+
+        ApiService.getAllTeammates.mockResolvedValue(resp);
+
+        wrapper = mount(App, {
+            data: () => {
+                return {
+                    teammates: [],
+                    newTeammate: {}
+                }
+            }
+        });
+    });
+
+    it("it populates the newTeammate object", () => {
+        const teammateToUpdate = teammates[0].id;
+
+        wrapper.findAllComponents(Card).wrappers[0]
+            .vm.updateTeammate(teammateToUpdate);
+
+        expect(wrapper.vm.newTeammate)
+            .toEqual(newTeammate);
     });
 
 });
