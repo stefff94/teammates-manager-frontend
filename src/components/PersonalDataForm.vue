080242ad28972fe15c4e8c4b26058a0bc7ad8581
Pol52 <innocentipaolo1@gmail.com>
2020-07-09 15:36:48 +0200

    added first row's design to PersonalDataForm.vue

--- src/components/PersonalDataForm.vue
+++ src/components/PersonalDataForm.vue
@@ -3,6 +3,22 @@
         <h3 class="ui dividing header">Teammate Data</h3>
         <div class="field">
             <label>Personal Data</label>
+            <div class="three fields">
+                <div class="field">
+                    <input type="text" name="name" placeholder="Name">
+                </div>
+                <div class="field">
+                    <input type="text" name="email" placeholder="E-mail">
+                </div>
+                <div class="field">
+                    <select class="ui selection dropdown" id="gender-dropdown" v-model="teammate.gender.value">
+                        <option value="" disabled selected>Gender</option>
+                        <option v-for="g in genders" v-bind:key="g.id" v-bind:value="g.id">
+                            {{ g.name }}
+                        </option>
+                    </select>
+                </div>
+            </div>
         </div>
     </form>
 </template>
