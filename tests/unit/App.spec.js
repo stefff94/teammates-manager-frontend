a32cfee53203224cd0dcb2b0965bee91d462846d
Pol52 <innocentipaolo1@gmail.com>
2020-07-10 10:43:29 +0200

    added failing tests to check if PersonalDataForm is in App.vue

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,9 +1,30 @@
 import { shallowMount } from '@vue/test-utils'
 import App from "../../src/App";
+import PersonalDataForm from "../../src/components/PersonalDataForm";
 
 let wrapper = null;
+let teammate = null;
+
 
 beforeEach(() => {
+    teammate = {
+        name: {
+            value: 'Name'
+        },
+        gender: {
+            value: 'M'
+        },
+        email: {
+            value: 'email@email.it'
+        },
+        city: {
+            value: 'City'
+        },
+        role: {
+            value: 'R1'
+        }
+    }
+
     wrapper = shallowMount(App);
 });
 
@@ -14,4 +35,20 @@ describe('App.vue', () => {
             .exists())
             .toBeTruthy();
     })
+
+    it('renders the PersonalDataForm', () => {
+        wrapper.setData({newTeammate: teammate})
+        const personalDataForm = wrapper.findComponent(PersonalDataForm);
+
+        expect(personalDataForm
+            .exists())
+            .toBeTruthy();
+        expect(personalDataForm.props('teammate'))
+            .toBe(wrapper.vm.$data.newTeammate);
+        expect(personalDataForm.props('genders'))
+            .toBe(wrapper.vm.$data.genders);
+        expect(personalDataForm.props('roles'))
+            .toBe(wrapper.vm.$data.roles);
+    })
+
 });
