d69dd4682ed94bdc7fe87439948c6f1869a99d33
Pol52 <innocentipaolo1@gmail.com>
2020-07-10 10:44:51 +0200

    added PersonalDataForm to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,38 @@
 <template>
   <div id="app">
-    <div class="mt35">
-    <tag-multiselect></tag-multiselect>
-    </div>
-
+    <personal-data-form :teammate="newTeammate" :genders="genders" :roles="roles"></personal-data-form>
     <div class="ui divider"></div>
 
   </div>
 </template>
 
 <script>
-import TagMultiselect from "./components/TagMultiselect";
+import PersonalDataForm from "./components/PersonalDataForm";
 export default {
   name: 'App',
-  components: {TagMultiselect}
-
+  components: {PersonalDataForm},
+  data: function() {
+    return{
+      newTeammate: {
+        name: {},
+        gender: {},
+        email: {},
+        city: {},
+        role: {}
+      },
+      roles: [
+        {id: 'R1', name: 'Student'},
+        {id: 'R2', name: 'Frontend developer'},
+        {id: 'R3', name: 'Backend developer'},
+        {id: 'R4', name: 'Full Stack developer'},
+        {id: 'R5', name: 'Analyst Programmer'},
+      ],
+      genders: [
+        {id: 'M', name: 'Male'},
+        {id: 'F', name: 'Female'}
+      ]
+    }
+  }
 }
 </script>
 
