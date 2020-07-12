bb771aa918c0626df9cefe51de54cce28fdc8590
Pol52 <innocentipaolo1@gmail.com>
2020-07-12 11:05:03 +0200

    added two integration tests for App, TagMultiselect and PersonalDataForm

---
+++ tests/it/AppIT.spec.js
@@ -0,0 +1,194 @@
+import { mount } from "@vue/test-utils";
+import App from "../../src/App";
+import ApiService from "../../src/services/api.service";
+import PersonalDataForm from "../../src/components/PersonalDataForm";
+import jQuery from "jquery";
+import $ from "jquery";
+import {avatarBaseUrl, avatars, genders, roles, rules} from "../../src/variables";
+import TagMultiselect from "../../src/components/TagMultiselect";
+import Multiselect from "vue-multiselect";
+
+global.jQuery = jQuery
+global.$ = $
+require('fomantic-ui/dist/semantic.min.js')
+
+
+let newTeammate = null;
+let wrapper = null;
+
+jest.mock("../../src/services/api.service");
+
+beforeEach(() => {
+
+    newTeammate = {
+        name: {
+            value: "Stefano Vannucchi"
+        },
+        gender: {
+            value: "M"
+        },
+        email: {
+            value: "stefano.vannucchi@stud.unifi.it"
+        },
+        city: {
+            value: "Prato"
+        },
+        role: {
+            value: "R1"
+        },
+        skills: [
+            { code: "Ja9000000", name: "Java" },
+            { code: "Sp9000000", name: "Spring Boot" }
+        ]
+    }
+
+    const mockMath = Object.create(global.Math)
+    mockMath.random = () => 0.9;
+    global.Math = mockMath;
+});
+
+describe('The teammate is saved after pressing submit', () => {
+    beforeEach(() => {
+        const resp = { data: {
+                id: 1
+            }};
+
+        ApiService.insertTeammate.mockResolvedValue(resp);
+
+        let respGetSkills = [
+            { id: 1, name: "Java" },
+            { id: 2, name: "Spring Boot" }
+        ]
+        ApiService.getSkills.mockResolvedValue(respGetSkills);
+
+        wrapper = mount(App,{
+            data: function() {
+                return{
+                    skills: [],
+                    newTeammate: {
+                        name: {},
+                        gender: {},
+                        email: {},
+                        city: {},
+                        role: {},
+                        skills: [],
+                        errors: []
+                    },
+                    roles: roles,
+                    genders: genders,
+                    avatars: avatars,
+                    teammates: [],
+                    rules: rules
+                }
+            }
+        });
+    });
+
+    it('saves the teammate data from the forms', async () => {
+
+        let personalDataForm = wrapper.findComponent(PersonalDataForm);
+
+        const nameInputField = personalDataForm.findAll('.three.fields input').at(0)
+        nameInputField.setValue(newTeammate.name.value)
+
+        const emailInputField = personalDataForm.findAll('.three.fields input').at(1)
+        emailInputField.setValue(newTeammate.email.value)
+
+        const genderSelectField = personalDataForm.find(".three.fields .field:nth-of-type(3) select");
+        genderSelectField.findAll('option')
+            .at(1)
+            .element
+            .selected = true;
+        genderSelectField.trigger('change');
+
+        const cityInputField = personalDataForm.findAll('.two.fields input').at(0)
+        cityInputField.setValue(newTeammate.city.value)
+
+        const roleSelectField = personalDataForm.find(".two.fields select");
+        roleSelectField.findAll('option')
+            .at(1)
+            .element
+            .selected = true;
+        roleSelectField.trigger('change');
+
+        const tagMultiselect = wrapper.findComponent(TagMultiselect);
+        const multiselect = tagMultiselect.findComponent(Multiselect);
+
+        newTeammate.skills.forEach(s => {
+            multiselect.vm.$emit('tag', s.name);
+        })
+
+        const expectedTeammate = {
+            id: 1,
+            personalData: {
+                name: newTeammate.name.value,
+                role: wrapper.vm.roles.find(r => {
+                    return r.id === newTeammate.role.value
+                }).name,
+                gender: newTeammate.gender.value,
+                photoUrl: avatarBaseUrl
+                    + wrapper.vm.$data.avatars[newTeammate.gender.value][2]
+                ,
+                email: newTeammate.email.value,
+                city: newTeammate.city.value
+            },
+            skills: [
+                {"code": "Ja9000000", "name": "Java"},
+                {"code": "Sp9000000", "name": "Spring Boot"}
+            ]
+        }
+
+        await wrapper.vm.$nextTick();
+
+        wrapper.find('button.ui.button:nth-of-type(1)').trigger('click');
+        await wrapper.vm.$nextTick();
+
+        expect(wrapper.vm.teammates)
+            .toContainEqual(expectedTeammate);
+    })
+})
+
+describe('the skills are added to App.skills', () => {
+    beforeEach(() => {
+
+        ApiService.getSkills.mockResolvedValue([]);
+
+        wrapper = mount(App,{
+            data: function() {
+                return{
+                    skills: [],
+                    newTeammate: {
+                        name: {},
+                        gender: {},
+                        email: {},
+                        city: {},
+                        role: {},
+                        skills: [],
+                        errors: []
+                    },
+                    roles: roles,
+                    genders: genders,
+                    avatars: avatars,
+                    teammates: [],
+                    rules: rules
+                }
+            }
+        });
+    })
+
+    it('adds the skill to the options', () => {
+        const tagMultiselect = wrapper.findComponent(TagMultiselect);
+        const newSkill = {code: 'sk9000000', name:'skill'}
+
+        tagMultiselect.vm.addSkill(newSkill.name);
+
+        expect(wrapper.vm.skills.length)
+            .toBe(1);
+        expect(wrapper.vm.newTeammate.skills.length)
+            .toBe(1);
+        expect(wrapper.vm.skills)
+            .toContainEqual(newSkill);
+        expect(wrapper.vm.newTeammate.skills)
+            .toContainEqual(newSkill);
+    })
+})
