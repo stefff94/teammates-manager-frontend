95296fa9b3a0455219ceb2f1b28a258e10bac5cc
Pol52 <innocentipaolo1@gmail.com>
2020-07-13 11:05:27 +0200

    improved test for TagMultiselect to check that the skill is correctly added to the teammate

--- tests/unit/components/TagMultiselectTest.spec.js
+++ tests/unit/components/TagMultiselectTest.spec.js
@@ -92,10 +92,6 @@ describe('TagMultiselect.vue', () => {
             .toMatch(skill.name);
         expect(wrapper.vm.options[0].id)
             .toMatch(skill.id);
-        expect(wrapper.vm.teammate.skills[0].name)
-            .toMatch(skill.name);
-        expect(wrapper.vm.teammate.skills[0].id)
-            .toMatch(skill.id)
     })
 })
 
